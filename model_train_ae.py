5062a297d45158fb5d86039d8f0b1f2cc7e2585c
Caglan <cakpinar23@gmail.com>
2020-02-29 12:55:22 +0300

    generic train model and feature editing from .json
    you can add a new model of .py file with its class and prediction and train model. All you need to add a new model into the learning_models.json file with a new Obj.
    model_processor.py is added which handles the prediction and training process of models at learning_models.json file
    you can assign the model of output column at the test set from learning_models.json and by using pred_fields and by using uid you may show with which columns you want to merge into existed data set.

--- model_train_ae.py
+++ model_train_ae.py
@@ -1,35 +1,36 @@
-from pandas import Series
-import datetime
-from tensorflow.keras.models import Model
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import RMSprop
-from tensorflow.keras.models import model_from_json
-from tensorflow.keras.callbacks import TensorBoard
 
-from configs import date_col, train_end_date, features_data_path, auto_encoder_model_paths, feature_path
-from data_access import get_data, decide_feature_name
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.utils import multi_gpu_model
+
+from configs import date_col, train_end_date, features_data_path, auto_encoder_model_paths, feature_path, run_gpu
+from data_access import decide_feature_name
+from data_access import get_data
 from logger import get_time
 
 
-class ModelTrainAutoEncoder:
-    def __init__(self, hyper_parameters=None, last_day_predictor=None, test_data=None, is_multivariate=None):
+class ModelTrainAutoEncoders:
+    def __init__(self, hyper_parameters=None, test_data=None, last_day_predictor=None, params=None):
         get_time()
         self.data = get_data(features_data_path, True)
         self.features = list(decide_feature_name(feature_path).keys())
         self.tuned_parameters = hyper_parameters
         self.last_day_predictor = last_day_predictor
+        self.model_params = params
         self.train, self.test = None, None if test_data is None else test_data
         self.X, self.y_pred, self.y = None, None, None
         self.input, self.fr_output = None, None
-        self.model_ae, self.model_ae_l = None, None
-        self.is_multivariate = is_multivariate
-        self.log_dir = "logs/fit/"
-        self.tensorboard_callback = {}
-
-    def model_logs(self, m_name):
-        self.tensorboard_callback = TensorBoard(log_dir=self.log_dir + m_name + '_' + str(datetime.datetime.now())[0:10],
-                                                histogram_freq=1)
+        self.model_ae, self.model_ae_l, self.model_u = None, None, None
+        self.gpu_devices = [d for d in device_lib.list_local_devices() if d.device_type == "GPU"] if run_gpu else []
 
     def model_from_to_json(self, path, model=None, is_writing=False):
         if is_writing:
@@ -56,28 +57,24 @@ class ModelTrainAutoEncoder:
 
     def auto_encoder(self):
         self.input = Input(shape=(len(self.features),))
-        encoder1 = Dense(200, activation='tanh')(self.input)
-        encoder2 = Dense(100, activation='tanh')(encoder1)
-        encoder3 = Dense(50, activation='tanh')(encoder2)
-        encoder4 = Dense(10, activation='tanh')(encoder3)
-        self.fr_output = Dense(1, activation='tanh')(encoder4)
-        decoder1 = Dense(10, activation='tanh')(self.fr_output)
-        decoder2 = Dense(50, activation='tanh')(decoder1)
-        decoder3 = Dense(100, activation='tanh')(decoder2)
-        decoder4 = Dense(200, activation='tanh')(decoder3)
-        decoder5 = Dense(len(self.features), activation='sigmoid')(decoder4)
-        self.model_ae = Model(inputs=self.input, outputs=decoder5)
-        self.model_ae.compile(loss='mse', optimizer=RMSprop(learning_rate=0.01), metrics=['mse'])
-
-    def auto_encoder_linear(self):
-        self.input = Input(shape=(len(self.features),))
-        self.fr_output = Dense(1, activation='relu')(self.input)
-        decoder = Dense(len(self.features), activation='sigmoid')(self.fr_output)
-        self.model_ae_l = Model(inputs=self.input, outputs=decoder)
-        self.model_ae_l.compile(loss='mse', optimizer=RMSprop(learning_rate=0.01), metrics=['mse'])
-
-    def feature_reduction(self):
-        return Model(inputs=self.input, outputs=self.fr_output)
+        encoder1 = Dense(1000, activation='tanh')(self.input)
+        encoder2 = Dense(500, activation='tanh')(encoder1)
+        encoder3 = Dense(100, activation='tanh')(encoder2)
+        encoder4 = Dense(50, activation='tanh')(encoder3)
+        encoder5 = Dense(10, activation='tanh')(encoder4)
+        code = Dense(len(self.features), activation='relu')(encoder5)
+        decoder1 = Dense(len(self.features), activation='relu')(code)
+        decoder2 = Dense(10, activation='tanh')(decoder1)
+        decoder3 = Dense(50, activation='tanh')(decoder2)
+        decoder4 = Dense(100, activation='tanh')(decoder3)
+        decoder5 = Dense(500, activation='tanh')(decoder4)
+        decoder6 = Dense(1000, activation='tanh')(decoder5)
+        self.fr_output = Dense(len(self.features), activation='sigmoid')(decoder6)
+        self.model_ae = Model(inputs=self.input, outputs=self.fr_output)
+        if len(self.gpu_devices) != 0:
+            self.model_ae = multi_gpu_model(self.model_ae, gpus=len(self.gpu_devices))
+        self.model_ae.compile(loss='mse', optimizer=RMSprop(lr=0.001), metrics=['mse'])
+        print(self.model_ae.summary())
 
     def model_train(self):
         print("Auto Encoder is initialized!!")
@@ -85,28 +82,27 @@ class ModelTrainAutoEncoder:
         self.train_test_split()
         self.get_x_values(is_for_prediction=False)
         self.auto_encoder()
-        self.auto_encoder_linear()
-        self.model_logs('ae')
-        self.model_ae.fit(self.X, self.X,
-                          epochs=int(self.tuned_parameters['feature_reduction']['epochs']),
-                          batch_size=int(self.tuned_parameters['feature_reduction']['batch_size']),
-                          validation_split=0.2, callbacks=[self.tensorboard_callback])
-        self.model_from_to_json(auto_encoder_model_paths['ae'], self.feature_reduction(), is_writing=True)
-        self.model_logs('ae_l')
-        self.model_ae_l.fit(self.X, self.X,
-                            epochs=int(self.tuned_parameters['feature_reduction']['epochs']),
-                            batch_size=int(self.tuned_parameters['feature_reduction']['batch_size']),
-                            validation_split=0.2, callbacks=[self.tensorboard_callback])
-        self.model_from_to_json(auto_encoder_model_paths['ae_l'], self.feature_reduction(), is_writing=True)
+        if len(self.gpu_devices) != 0:
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+                config = tf.ConfigProto(log_device_placement=True)
+                self.model_ae.fit(self.X, self.X,
+                                  epochs=int(self.tuned_parameters['ae']['epochs']),
+                                  batch_size=int(self.tuned_parameters['ae']['batch_size']),
+                                  validation_split=0.2, shuffle=True)
+        else:
+            self.model_ae.fit(self.X, self.X,
+                              epochs=int(self.tuned_parameters['ae']['epochs']),
+                              batch_size=int(self.tuned_parameters['ae']['batch_size']),
+                              validation_split=0.2, shuffle=True)
+        self.model_from_to_json(auto_encoder_model_paths['ae'], self.model_ae, is_writing=True)
 
-    def calculating_loss_function(self, is_for_prediction):
-        self.model_ae = self.model_from_to_json(auto_encoder_model_paths['ae'], is_writing=False)
-        self.model_ae_l = self.model_from_to_json(auto_encoder_model_paths['ae_l'], is_writing=False)
+    def calculating_loss_function(self):
+        self.train_test_split()
         self.get_x_values(is_for_prediction=True)
-        self.y_pred = self.model_ae.predict(self.X)
-        self.y = self.model_ae_l.predict(self.X)
-        # AE works better when we try to find out the outliers
-        anomaly_calculations = list(map(lambda x: abs(x[0] - x[1])[0], zip(self.y_pred, self.y)))
-        self.test['anomaly_ae_values'] = Series(anomaly_calculations)
-        self.test.to_csv("prediction.csv", index=False)
+        self.model_ae = self.model_from_to_json(auto_encoder_model_paths['ae'], is_writing=False)
+        self.y_pred, self.y = self.model_ae.predict(self.X), self.X
+        anomaly_calculations = list(map(lambda x: np.mean([abs(x[0][f] - x[1][f]) for f in range(len(self.features))]),
+                                        zip(self.y_pred, self.y)))
+        self.test[self.model_params['args']['pred_field']] = pd.Series(anomaly_calculations)
 
