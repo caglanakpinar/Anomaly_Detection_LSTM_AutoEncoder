a7fcc0c5093c063e0db0c7c459c49257109781f5
Caglan <cakpinar23@gmail.com>
2020-03-03 23:59:44 +0300

    dag of tasks dependecies are rearranged

--- dags.py
+++ dags.py
@@ -4,8 +4,8 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 
 from main_airflow import main
-from data_access import decide_feature_name, model_from_to_json
-from configs import feature_path, learning_model_path
+from data_access import model_from_to_json
+from configs import learning_model_path
 
 
 
@@ -29,18 +29,6 @@ bash_2 = BashOperator(task_id='fe_ends', bash_command='echo start', dag=dag)
 # train ends
 bash_3 = BashOperator(task_id='train_ends', bash_command='echo start', dag=dag)
 
-task1 = PythonOperator(task_id='fe',
-                       op_kwargs={'args': ['main.py', 'feature_engineering', '0', 'all']},
-                       python_callable=main,
-                       dag=dag
-                       )
-
-task2 = PythonOperator(task_id='train',
-                       op_kwargs={'args': ['main.py', 'train_process', '0', 'all']},
-                       python_callable=main,
-                       dag=dag
-                       )
-
 task3 = PythonOperator(task_id='prediction',
                        op_kwargs={'args': ['main.py', 'prediction', '0', 'all']},
                        python_callable=main,
@@ -54,16 +42,12 @@ task4 = PythonOperator(task_id='dashboard',
                        )
 
 # Feature Engineering
-count = 0
-for f in decide_feature_name(feature_path):
-    _task_1 = PythonOperator(task_id='fe_' + str(count),
-                             op_kwargs={'args': ['main.py', 'feature_engineering', '0', f]},
-                             python_callable=main,
-                             dag=dag
-                             )
-    bash_1 >> _task_1
-    _task_1.set_downstream(bash_2)
-    count += 1
+task_1 = PythonOperator(task_id='feature_engineering',
+                        op_kwargs={'args': ['main.py', 'feature_engineering', 'all']},
+                        python_callable=main,
+                        dag=dag
+                        )
+bash_1 >> task_1 >> bash_2
 
 # Train Process
 count = 0
@@ -77,4 +61,4 @@ for m in model_from_to_json(learning_model_path, [], False):
     _task_2.set_downstream(bash_3)
     count += 1
 
-
+bash_3 >> task3 >> task4
